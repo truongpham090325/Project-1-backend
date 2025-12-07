f47864e89beac07ea62ce4984ce6eb003e69a377
Phạm Quang Trường <truongpham090325@gmail.com>
2025-12-07 15:25:05 +0700

    05. Tích hợp PUG

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
+// Thiết lập thư mục view
+app.set("views", path.join(__dirname, "views"));
+
+// Thiết lập thư mục chứa template engine
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.send("Trang chủ!");
+  res.render("client/pages/home", {
+    pageTitle: "Trang chủ",
+  });
 });
 
 app.get("/tours", (req, res) => {
-  res.send("Trang danh sách tour");
+  res.render("client/pages/tour-list", {
+    pageTitle: "Trang danh sách tour",
+  });
 });
 
 app.listen(port, () => {
