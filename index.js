3e78f066c1eb281d7d25ba8c0f3c05ec77b4547c
Phạm Quang Trường <truongpham090325@gmail.com>
2025-12-07 15:06:53 +0700

    03. Khởi tạo Project 1

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Trang chủ!");
+});
+
+app.get("/tours", (req, res) => {
+  res.send("Trang danh sách tour");
+});
+
+app.listen(port, () => {
+  console.log(`Website đang chạy ở cổng ${port}`);
+});
