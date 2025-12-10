3edc2d0bf36f951b79d39a65a22de5f1d53f8715
Phạm Quang Trường <truongpham090325@gmail.com>
2025-12-10 15:26:14 +0700

    1.12. Tích hợp giao diện trang Quản lý người dùng

---
+++ controllers/admin/user.controller.js
@@ -0,0 +1,5 @@
+module.exports.list = (req, res) => {
+  res.render("admin/pages/user-list", {
+    pageTitle: "Quản lý người dùng",
+  });
+};
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,11 +4,13 @@ const dashboardRoutes = require("./dashboard.route");
 const categoryRoutes = require("./category.route");
 const tourRoutes = require("./tour.route");
 const orderRoutes = require("./order.route");
+const userRoutes = require("./user.route");
 
 router.use("/account", accountRoutes);
 router.use("/dashboard", dashboardRoutes);
 router.use("/category", categoryRoutes);
 router.use("/tour", tourRoutes);
 router.use("/order", orderRoutes);
+router.use("/user", userRoutes);
 
 module.exports = router;
