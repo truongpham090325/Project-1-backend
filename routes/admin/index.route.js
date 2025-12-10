fd806aa6203c686cbc63cc8580c3f81c49263afa
Phạm Quang Trường <truongpham090325@gmail.com>
2025-12-10 14:51:18 +0700

    1.4. Tích hợp giao diện trang Tổng quan

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const accountRoutes = require("./account.route");
+const dashboardRoutes = require("./dashboard.route");
 
 router.use("/account", accountRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 module.exports = router;
