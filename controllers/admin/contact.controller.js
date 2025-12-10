d8ba1fe033b660cca5f4e87cdabc7723485c5d8c
Phạm Quang Trường <truongpham090325@gmail.com>
2025-12-10 15:31:23 +0700

    1.13. Tích hợp giao diện trang
    Thông tin liên hệ

---
+++ controllers/admin/contact.controller.js
@@ -0,0 +1,5 @@
+module.exports.list = (req, res) => {
+  res.render("admin/pages/contact-list", {
+    pageTitle: "Thông tin liên hệ",
+  });
+};
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,6 +5,7 @@ const categoryRoutes = require("./category.route");
 const tourRoutes = require("./tour.route");
 const orderRoutes = require("./order.route");
 const userRoutes = require("./user.route");
+const contactRoutes = require("./contact.route");
 
 router.use("/account", accountRoutes);
 router.use("/dashboard", dashboardRoutes);
@@ -12,5 +13,6 @@ router.use("/category", categoryRoutes);
 router.use("/tour", tourRoutes);
 router.use("/order", orderRoutes);
 router.use("/user", userRoutes);
+router.use("/contact", contactRoutes);
 
 module.exports = router;
