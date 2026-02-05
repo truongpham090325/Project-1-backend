7dc038671348f5142466ca16f46a4a238dc18696
Phạm Quang Trường <truongpham090325@gmail.com>
2026-02-05 14:19:25 +0700

    Làm trang Tạo tour

--- controllers/admin/tour.controller.js
+++ controllers/admin/tour.controller.js
@@ -1,13 +1,95 @@
+const buildCategoryTree = require("../../helpers/category.helper");
+const Category = require("../../models/category-model");
+const City = require("../../models/city.model");
+const Tour = require("../../models/tour.model");
+
 module.exports.list = (req, res) => {
   res.render("admin/pages/tour-list", {
     pageTitle: "Quản lý tour",
   });
 };
 
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+  // Danh sách danh mục dạng cây
+  const categoryList = await Category.find({
+    deleted: "false",
+  });
+  const categoryTree = buildCategoryTree(categoryList, "");
+  // Hết danh sách danh mục dạng cây
+
+  // Danh sách thành phố
+  const cityList = await City.find({});
+  // Hết Danh sách thành phố
+
   res.render("admin/pages/tour-create", {
     pageTitle: "Tạo tour",
+    categoryList: categoryTree,
+    cityList: cityList,
   });
+};
+
+module.exports.createPost = async (req, res) => {
+  try {
+    if (req.body.position) {
+      req.body.position = parseInt(req.body.position);
+    } else {
+      const totalRecord = await Tour.countDocuments({});
+      req.body.position = totalRecord + 1;
+    }
+
+    req.body.avatar = req.file ? req.file.path : "";
+    req.body.priceAdult = req.body.priceAdult
+      ? parseInt(req.body.priceAdult)
+      : 0;
+    req.body.priceChildren = req.body.priceChildren
+      ? parseInt(req.body.priceChildren)
+      : 0;
+    req.body.priceBaby = req.body.priceBaby ? parseInt(req.body.priceBaby) : 0;
+    req.body.priceNewAdult = req.body.priceNewAdult
+      ? parseInt(req.body.priceNewAdult)
+      : 0;
+    req.body.priceNewChildren = req.body.priceNewChildren
+      ? parseInt(req.body.priceNewChildren)
+      : 0;
+    req.body.priceNewBaby = req.body.priceNewBaby
+      ? parseInt(req.body.priceNewBaby)
+      : 0;
+    req.body.stockAdult = req.body.stockAdult
+      ? parseInt(req.body.stockAdult)
+      : 0;
+    req.body.stockChildren = req.body.stockChildren
+      ? parseInt(req.body.stockChildren)
+      : 0;
+    req.body.stockBaby = req.body.stockBaby ? parseInt(req.body.stockBaby) : 0;
+
+    req.body.departureDate = req.body.departureDate
+      ? new Date(req.body.departureDate)
+      : null;
+
+    req.body.locations = req.body.locations
+      ? JSON.parse(req.body.locations)
+      : [];
+    req.body.schedules = req.body.schedules
+      ? JSON.parse(req.body.schedules)
+      : [];
+
+    req.body.createdBy = req.account.id;
+    req.body.updatedBy = req.account.id;
+
+    const newRecord = new Tour(req.body);
+    await newRecord.save();
+
+    res.json({
+      code: "success",
+      message: "Thêm tour thành công!",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      code: "error",
+      message: "Dữ liệu không hợp lệ!",
+    });
+  }
 };
 
 module.exports.trash = (req, res) => {
--- routes/admin/tour.route.js
+++ routes/admin/tour.route.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
 const tourController = require("../../controllers/admin/tour.controller");
+const tourValidate = require("../../validates/admin/tour.validate");
+const multer = require("multer");
+const cloudinaryHelper = require("../../helpers/cloudinary.helper");
+
+const upload = multer({ storage: cloudinaryHelper.storage });
 
 router.get("/list", tourController.list);
 
 router.get("/create", tourController.create);
+
+router.post(
+  "/create",
+  upload.single("avatar"),
+  tourValidate.createPost,
+  tourController.createPost,
+);
 
 router.get("/trash", tourController.trash);
 
