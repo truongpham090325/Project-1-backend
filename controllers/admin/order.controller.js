136f7913e3e5427f83128cf00548a020387e628e
Phạm Quang Trường <truongpham090325@gmail.com>
2026-03-17 09:33:55 +0700

    Làm trang Chỉnh sửa đơn hàng (Admin)

--- controllers/admin/order.controller.js
+++ controllers/admin/order.controller.js
@@ -1,9 +1,11 @@
 const Order = require("../../models/order.model");
 const Tour = require("../../models/tour.model");
+const City = require("../../models/city.model");
 const {
   paymentMethodList,
   paymentStatusList,
   statusList,
+  pathAdmin,
 } = require("../../config/variable.config");
 const moment = require("moment");
 
@@ -51,8 +53,74 @@ module.exports.list = async (req, res) => {
   });
 };
 
-module.exports.edit = (req, res) => {
-  res.render("admin/pages/order-edit", {
-    pageTitle: "Đơn hàng: OD000001",
-  });
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const orderDetail = await Order.findOne({
+      _id: id,
+    });
+
+    if (!orderDetail) {
+      res.redirect(`/${pathAdmin}/order/list`);
+      return;
+    }
+
+    orderDetail.createdAtFormat = moment(orderDetail.createdAt).format(
+      "YYYY-MM-DDTHH:mm",
+    );
+
+    for (const item of orderDetail.items) {
+      const infoTour = await Tour.findOne({
+        _id: item.tourId,
+      });
+
+      if (infoTour) {
+        item.avatar = infoTour.avatar;
+        item.name = infoTour.name;
+        item.departureDateFormat = moment(item.departureDate).format(
+          "DD/MM/YYYY",
+        );
+        const city = await City.findOne({
+          _id: item.locationFrom,
+        });
+        item.cityName = city.name;
+      }
+    }
+
+    res.render("admin/pages/order-edit", {
+      pageTitle: `Đơn hàng: ${orderDetail.code}`,
+      orderDetail: orderDetail,
+      paymentMethodList: paymentMethodList,
+      paymentStatusList: paymentStatusList,
+      statusList: statusList,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect(`/${pathAdmin}/order/list`);
+  }
+};
+
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Order.updateOne(
+      {
+        _id: id,
+        deleted: false,
+      },
+      req.body,
+    );
+
+    res.json({
+      code: "success",
+      message: "Cập nhật đơn hàng thành công!",
+    });
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Dữ liệu không hợp lệ!",
+    });
+  }
 };
--- routes/admin/order.route.js
+++ routes/admin/order.route.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const orderController = require("../../controllers/admin/order.controller");
+const orderValidate = require("../../validates/admin/order.validate");
 
 router.get("/list", orderController.list);
 
-router.get("/edit", orderController.edit);
+router.get("/edit/:id", orderController.edit);
+
+router.patch("/edit/:id", orderValidate.editPatch, orderController.editPatch);
 
 module.exports = router;
